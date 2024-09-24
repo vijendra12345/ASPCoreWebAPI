b0f0c51b07570c4d385fbd979b5e4366f8cb6a12
vijendra12345 <vijendra.singh0682@gmail.com>
2024-09-24 17:10:47 +0530

    remove commeted code

--- ASPCoreWebAPI/Program.cs
+++ ASPCoreWebAPI/Program.cs
@@ -8,9 +8,6 @@ namespace ASPCoreWebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
